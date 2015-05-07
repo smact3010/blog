f967ffbfa62d223a1889ff26aad3886447794db3
Stephan Taylor <stephan.taylor@irco.com>
2015-05-07 10:10:48 -0600

    5.8 Listing all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
   # in each controller in the following order:
   # index, show, new, edit, create, update and destroy
 
+  def index
+    @articles = Article.all  
+  end
+
   def show
   	@article = Article.find(params[:id])
   end
